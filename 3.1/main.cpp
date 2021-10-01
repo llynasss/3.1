b662024f5a5905466b5c886dbd60cae0fcaf274a
Magdalyna Khmyz <magdakhmyz@MacBook-Air-Magdalyna.local>
2021-10-01 10:02:03 +0300

    3.1

--- 3.1/main.cpp
+++ 3.1/main.cpp
@@ -1,14 +1,52 @@
-//
-//  main.cpp
-//  3.1
-//
-//  Created by Magdalyna Khmyz on 29.09.2021.
-//
+
+// Lab_03_1.cpp
+// < Хмиз Магдалина >
+// Лабораторна робота No 3.1
+// Розгалуження, задане формулою: функція однієї змінної. // Варіант 21
 
 #include <iostream>
+#include <cmath>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+int main()
+{
+    double x;  // вхідний параметр
+    double y; // результат обчислення виразу
+    double A; // проміжний результат - функціонально стала частина виразу
+    double B; // проміжний результат - функціонально змінна частина виразу
+
+    cout<<"x=";cin >> x;
+    A = 2+1/abs(1+x);
+       
+    
+    // спосіб 1: розгалуження в скороченій формі
+       if (x<1)
+           B = sqrt(abs(cos(x))+13);
+       if (1<=x && x<=5)
+           B = 3+1/tan(4+x/sqrt(2));
+       if (x>5)
+           B = 8+0.7*x;
+    
+        y =  A - B ;
+    
+       cout << endl;
+       cout << "1) y = " << y << endl;
+    
+    
+       // спосіб 2: розгалуження в повній формі
+       if (x<1)
+              B = sqrt(abs(cos(x))+13);
+       else
+          if (x>5)
+               B = 8+0.7*x ;
+          else
+               B = 3+1/tan(4+x/sqrt(2));
+       y = A - B;
+    
+       cout << "2) y = " << y << endl;
+    
+       cin.get();
+       return 0;
+    
 }
